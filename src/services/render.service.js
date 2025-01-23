6a388a3ad87f7355caca9c74f52887cc2423b1cf
Oksana Aleksandrova <102592873+oks767@users.noreply.github.com>
2025-01-23 14:25:45 +0300

    создание методов, которые заменяют теги на кастомные

--- src/services/render.service.js
+++ src/services/render.service.js
@@ -1,19 +1,43 @@
 import htmlFile from './index.html'
 class RenderService extends HTMLElement {
-	get html() {
-		return this.innerHTML;
+
+	htmlToElement(html, components = [], styles) {
+		const template = document.createElement('template');
+		template.innerHTML = html;
+
+		this.#replaceComponentTag
+		this.#applyModuleStyles(styles);
+		return template.content.firstChild;
 	}
 	
-	set html(value) {
-		this.innerHTML = value;
+	#replaceComponentTag(parentElement, components) {
+		const componentTagPattern = /^component-/;
+		const allElements = parentElement.getElementByTagName('*');
+		for (const element of allElements) {
+			if (componentTagPattern.test(element.tagName.toLowerCase())) {
+				const componentName = element.tagName.toLowerCase().replace(componentTagPattern, '').replace(/-/g, '');
+				// найти компонент в массиве компонентов в текущей итерации цикла
+				const foundComponent = components.find(Component => {
+					const instance = Component instanceof ChildComponent ? Component : new Component();
+					return instance.constructor.name.toLowerCase() === componentName;
+				});
+				// если компонент найден, заменить элемент на содержимое компонента
+				if(foundComponent) {
+					const componentContent = foundComponent instanceof ChildComponent ? foundComponent : new foundComponent().render();
+					element.replaceWith(componentContent);
+				} else {
+					console.error(`Component ${componentName} not found`);
+				}
+			}
+		}
 	}
-	
-	connectedCallback() {
-		this.render();
-	}
-	
-	render() {
-		this.innerHTML = this.html;
+	// create #applyModuleStyles
+	// будет принимать стили в виде объекта styles: {componentName: 'styles'}; нужно создать массив, пройтись по нему и добавлять уникальные значения к классу элемента
+	#applyModuleStyles (style) {
+		const styleElement = Object.entries(style).map(([componentName, styles]) => {
+			const componentReplacePattern = new RegExp(`component-${componentName}`, 'g');
+			return styles.replace(componentReplacePattern, '');
+	}, '');	
+	styleElement.innerHTML = styleElement;
 	}
 }
-customElements.define(htmlFile, RenderService)
